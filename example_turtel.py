fb98ee0b468750df097c646e78d7ad1e100f1b52
hamedpython2020 <karimihamedmi@gmail.com>
2023-01-15 23:37:35 +0330

    .

--- example_turtel.py
+++ example_turtel.py
@@ -59,11 +59,20 @@ while True:
     tr.fd(i)
     pass
     ##################### drow Finish ########################
-x2, y2 = (-1*i, i)
-x1, y1 = (-10, 10)
+x1, y1 = (-10, 10)              #X1#
+x2, y2 = (-1*i, i)              #X2#
+x3, y3 = (110, 10)              #X3#
+x4, y4 = (100+i, i)             #X4#
+x5, y5 = (110, -10)             #X5#
+x6, y6 = (100+i, -1*i)          #X6#
+x7, y7 = (-10, -10)             #X7#
+x8, y8 = (-1*i, -1*i)           #X8#
 dx2 = (i+100+i) / 10
 dx1 = ((-2*x1) + 100) / 10
 print(x1, y1, x2, y2)
+print(x3, y3, x4, y4)
+print(x5, y5, x6, y6)
+print(x7, y7, x8, y8)
 n = 0
 d1 = 0
 d2 = 0
@@ -87,24 +96,3 @@ while n <= 10:
     helper = dx2
     pass
 #################Down join######################
-# helper = 0
-# tr.goto(x2,-1*y2)
-# yd1 = -1 * y1
-# yd2 = -1*y2
-# n = 0
-# while n <= 10:
-#     n += 1
-#     if n % 2 != 0:
-#         tr.fd(helper)
-#         tr.color("blue")
-#         tr.goto(x1+d1, yd1)
-#     else:
-#         tr.fd(dx1)
-#         tr.color("purple")
-#         tr.goto(x2 + d2, yd2)
-#         pass
-#     tr.color("white")
-#     d1 += dx1
-#     d2 += dx2
-#     helper = dx2
-#     pass
